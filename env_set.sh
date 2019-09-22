1c9486bc2b8a7cc3c136589f4a8cd07d660ef20b
sanghunkang <sanghunkang.dev@gmail.com>
2019-09-23 00:28:28 +0900

    having fun, potentially serious

---
+++ env_set.sh
@@ -0,0 +1,10 @@
+# The purpose of this script to ensure that I work on the same environmet for each project
+
+# Scala
+
+
+# flutter
+# cd /usr/local
+unzip ~/Downloads/flutter_macos_v1.9.1+hotfix.2-stable.zip
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -license
